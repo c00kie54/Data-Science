cb53b8dcbd8ae30f3854be3a8c94ba5cabc3eeb8
AlexCooke <alex.pj.cooke@gmail.com>
2020-09-17 17:40:56 +0100

    Adding code for the json 2018/2019 clean up. The outpu needs to be merged with the clean 2018/2018 player data.

--- json_clean18.py
+++ json_clean18.py
@@ -1,1 +1,9 @@
 #This script will clean the json needed for the 2018/2019 dataset
+
+def json_clean18(json_data18):
+    json_data18.drop(json_data18.columns.difference(['Club','Position']), 1, inplace=True)
+    json_data18 =json_data18.reset_index()
+    json_data18.columns = ['name', 'Club','Position']
+    json_data18 =json_data18.sort_values(by=['name'])
+    json_data18 = json_data18.replace({' ':''}, regex=True)
+    return json_data18

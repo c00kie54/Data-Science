8daad71ce1ca7c0e1b47af35426c62e8d87eaeb0
AlexCooke <alex.pj.cooke@gmail.com>
2020-09-17 17:31:08 +0100

    Adding the function to the script.

--- json_load.py
+++ json_load.py
@@ -1,1 +1,9 @@
 #This script will load JSON and if the shape(m,n) is m<n then it will transpose it
+
+def json_load(json_name):
+    data = pd.read_json (json_name) #Read JSON
+    m,n = data.shape #Provides two variables of the shape
+    if n>m: #IF n>m then data needs to be transposed.
+        data = data.T
+        return data
+    return data
